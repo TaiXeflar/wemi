2c2b03d30a1b319b0f2b8a94978e007307607243
TaiXeflar <dcoocs123@gmail.com>
2026-06-09 20:00:28 +0800

    Improved miHoYo SDK rules

--- SDKs/find_MiHoYo.py
+++ SDKs/find_MiHoYo.py
@@ -7,10 +7,12 @@ import re
 from pathlib import Path
 
 from utils import regedit, message
+from tasks import ModulesObject
 from .refs import FindSDK
 from .refs._findMHY import (
     MIHOYO_PROJECTS_TYPEHINT,
     mihoyo_app_registry_phonebook,
+    mihoyo_cn_app_registry_phonebook,
     mihoyo_app_include_file_dict,
 )
 
@@ -26,34 +28,102 @@ class FindMiHoYo(FindSDK):
     def __WINDOWS__(self):
         k: MIHOYO_PROJECTS_TYPEHINT
 
-        for k, v in mihoyo_app_registry_phonebook.items():
-            install_dir = regedit("HKCU", v, key_name="GameInstallPath")
+        hoyo_launcher_path = regedit("HKCU", r'Software\Cognosphere\HYP\1_0', key_name='InstallPath')
+        hoyo_mi_launcher_path = regedit("HKCU", r'Software\miHoYo\HYP\1_1', key_name='InstallPath')
 
-            if install_dir is None:
-                continue
-            else:
-                install_dir = Path(install_dir).resolve()
-                pth = install_dir.as_posix()
-                version_file = Path(install_dir / "config.ini").resolve()
+        if hoyo_launcher_path:
+            _pth = Path(hoyo_launcher_path).as_posix()
+            message(f'    Found MiHoYo/HoYoVerse Launcher (Global)      {_pth}')
+            self.add_rule(ModulesObject(
+                Module='miHoYo/HoYoVerse',
+                output='miHoYo/HoYoVerse',
+                mode='tcl',
+                module_whaits='MiHoYo/HoYoVerse Launcher (Global)',
+                Include_file='template_mihoyo_hoyoverse',
+                root=_pth,
+                PATH=['$root'],
+                MODULEPATH=['.deps/miHoYo/HoYoVerse']
+            ))
 
-                with open(version_file, "r", encoding="utf-8") as f:
-                    config_content = f.read().strip()
-                    version_info = re.search(
-                        r"version=(\d+\.\d+\.\d+)", config_content
-                    ).group(1)
-                    major, minor, _ = map(int, version_info.split("."))
-                    version = (
-                        f"{major}.{minor}" + f" (Luna-{minor+1})"
-                        if major == 6 and k == "GenshinImpact"
-                        else f"{major}.{minor}"
-                    )
+            for k, v in mihoyo_app_registry_phonebook.items():
 
-            _fmt = "    Project " + f"{k:<15}" + f"{version:<15}" + pth
-            message(_fmt)
-            self.add_rule(
-                Module=f"miHoYo/{k}",
-                mode="tcl",
-                Include_file=mihoyo_app_include_file_dict[k],
-                root=pth,
-                PATH=["$root"],
-            )
+                install_dir = regedit("HKCU", v, key_name="GameInstallPath")
+                if install_dir is None:
+                    continue
+                else:
+                    install_dir = Path(install_dir).resolve()
+                    pth = install_dir.as_posix()
+                    version_file = Path(install_dir / "config.ini").resolve()
+
+                    with open(version_file, "r", encoding="utf-8") as f:
+                        config_content = f.read().strip()
+                        version_info = re.search(
+                            r"version=(\d+\.\d+\.\d+)", config_content
+                        ).group(1)
+                        major, minor, _ = map(int, version_info.split("."))
+                        version = (
+                            f"{major}.{minor}" + f" (Luna-{minor+1})"
+                            if major == 6 and k == "GenshinImpact"
+                            else f"{major}.{minor}"
+                        )
+
+                _fmt = "    Project " + f"{k:<15}" + f"{version:<15}" + pth
+                message(_fmt)
+                proj, _, server = k.partition('.')
+                server = 'global' if not server else server
+                self.add_rule(
+                    Module=f"miHoYo/{proj}/{server}",
+                    output=f'.deps/miHoYo/HoYoVerse/miHoYo/{proj}/{server}',
+                    mode="tcl",
+                    Include_file=mihoyo_app_include_file_dict[proj],
+                    root=pth,
+                    PATH=["$root"],
+                )
+
+        if hoyo_mi_launcher_path:
+            _pth = Path(hoyo_mi_launcher_path).as_posix()
+            message(f'    Found MiHoYo/MiHoYo Launcher (Mainland China)     {_pth}')
+            self.add_rule(ModulesObject(
+                Module='miHoYo/miHoYo',
+                output='miHoYo/miHoYo',
+                mode='tcl',
+                module_whaits='MiHoYo/MiHoYo Launcher (Mainland China)',
+                Include_file='template_mihoyo_hoyoverse',
+                root=_pth,
+                PATH=['$root'],
+                MODULEPATH=['.deps/miHoYo/miHoYo']
+            ))
+            for k, v in mihoyo_cn_app_registry_phonebook.items():
+                install_dir = regedit("HKCU", v, key_name="GameInstallPath")
+
+                if install_dir is None:
+                    continue
+                else:
+                    install_dir = Path(install_dir).resolve()
+                    pth = install_dir.as_posix()
+                    version_file = Path(install_dir / "config.ini").resolve()
+
+                    with open(version_file, "r", encoding="utf-8") as f:
+                        config_content = f.read().strip()
+                        version_info = re.search(
+                            r"version=(\d+\.\d+\.\d+)", config_content
+                        ).group(1)
+                        major, minor, _ = map(int, version_info.split("."))
+                        version = (
+                            f"{major}.{minor}" + f" (Luna-{minor+1})"
+                            if major == 6 and k == "GenshinImpact"
+                            else f"{major}.{minor}"
+                        )
+
+                _fmt = "    Project " + f"{k:<15}" + f"{version:<15}" + pth
+                message(_fmt)
+                proj, _, server = k.partition('.')
+                server = 'global' if not server else server
+                self.add_rule(
+                    Module=f"miHoYo/{proj}/{server}",
+                    output=f'.deps/miHoYo/miHoYo/miHoYo/{proj}/{server}',
+                    mode="tcl",
+                    Include_file=mihoyo_app_include_file_dict[proj],
+                    root=pth,
+                    PATH=["$root"],
+                )
--- SDKs/refs/_findMHY.py
+++ SDKs/refs/_findMHY.py
@@ -3,55 +3,74 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+# SPDX-License-Identifier: MIT
+# Copyright.c 2026-${year} WEMI Contributors
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 from typing import Literal
 
 MIHOYO_PROJECTS_TYPEHINT = Literal[
     "FlyMeToTheMoon",
-    "GunsGirlZ (CN)",
-    "GunsGirlZ (JP)",
-    "HonkaiImpact3 (ZH)",
-    "HonkaiImpact3 (JP)",
-    "HonkaiImpact3 (KR)",
-    "HonkaiImpact3 (SEA)",
-    "HonkaiImpact3 (EU_USA)",
-    "GenshinImpact",
-    "HonkaiStarRail",
-    "ZenlessZoneZero",
-    "HonkaiNexusAnima",
+    "BH2.CN",
+    "BH2.JP",
+    "BH3.ZH",
+    "BH3.JP",
+    "BH3.KR",
+    "BH3.SEA",
+    "BH3.EU_USA",
+    "Genshin",
+    "HKSR",
+    "ZZZ",
+    "HNA",
+    "PPT",
     "Varzapura",
-    "TearsOfThemis",
 ]
 
 mihoyo_app_registry_phonebook = {
     "FlyMeToTheMoon": None,
-    "GunsGirlZ (CN)": None,
-    "GunsGirlZ (JP)": None,
-    "HonkaiImpact3 (ZH)": r"Software\Cognosphere\HYP\1_0\bh3_globalasia_official",
-    "HonkaiImpact3 (JP)": r"Software\Cognosphere\HYP\1_0\bh3_globaljp_official",
-    "HonkaiImpact3 (KR)": r"Software\Cognosphere\HYP\1_0\bh3_globalkr_official",
-    "HonkaiImpact3 (SEA)": r"Software\Cognosphere\HYP\1_0\bh3_globaloverseas_official",
-    "HonkaiImpact3 (EU_USA)": r"Software\Cognosphere\HYP\1_0\bh3_globalglb_official",
-    "GenshinImpact": r"Software\Cognosphere\HYP\1_0\hk4e_global",
-    "HonkaiStarRail": r"Software\Cognosphere\HYP\1_0\hkrpg_global",
-    "ZenlessZoneZero": r"Software\Cognosphere\HYP\1_0\nap_global",
-    "HonkaiNexusAnima": None,
+    "BH2.CN": None,
+    "BH2.JP": None,
+    "BH3.ZH": r"Software\Cognosphere\HYP\1_0\bh3_globalasia_official",
+    "BH3.JP": r"Software\Cognosphere\HYP\1_0\bh3_globaljp_official",
+    "BH3.KR": r"Software\Cognosphere\HYP\1_0\bh3_globalkr_official",
+    "BH3.SEA": r"Software\Cognosphere\HYP\1_0\bh3_globaloverseas_official",
+    "BH3.EU_USA": r"Software\Cognosphere\HYP\1_0\bh3_globalglb_official",
+    "Genshin": r"Software\Cognosphere\HYP\1_0\hk4e_global",
+    "HKSR": r"Software\Cognosphere\HYP\1_0\hkrpg_global",
+    "ZZZ": r"Software\Cognosphere\HYP\1_0\nap_global",
+    "HNA": None,
+    "PPT": None,
     "Varzapura": None,
-    "TearsOfThemis": None,
 }
+
+mihoyo_cn_app_registry_phonebook = {
+    "FlyMeToTheMoon": None,
+    "BH2.CN": None,
+    "BH2.JP": None,
+    "BH3.CN": r"Software\miHoYo\HYP\1_1\bh3_cn",
+    "Genshin": r"Software\Cognosphere\HYP\1_0\hk4e_cn",
+    "HKSR": r"Software\Cognosphere\HYP\1_0\hkrpg_cn",
+    "ZZZ": r"Software\Cognosphere\HYP\1_0\nap_cn",
+    "HNA": None,
+    "PPT": None,
+    "Varzapura": None,
+}
+
 
 mihoyo_app_include_file_dict = {
     "FlyMeToTheMoon": None,
-    "GunsGirlZ (CN)": r"template_mhy_bh2",
-    "GunsGirlZ (JP)": r"template_mhy_bh2",
-    "HonkaiImpact3 (ZH)": r"template_mhy_bh3",
-    "HonkaiImpact3 (JP)": r"template_mhy_bh3",
-    "HonkaiImpact3 (KR)": r"template_mhy_bh3",
-    "HonkaiImpact3 (SEA)": r"template_mhy_bh3",
-    "HonkaiImpact3 (EU_USA)": r"template_mhy_bh3",
-    "GenshinImpact": r"template_mhy_genshin",
-    "HonkaiStarRail": r"template_mhy_starrail",
-    "ZenlessZoneZero": r"template_mhy_zzz",
-    "HonkaiNexusAnima": r"template_mhy_nexusanima",
+    "BH2.CN": r"template_mhy_bh2",
+    "BH2.JP": r"template_mhy_bh2",
+    "BH3.ZH": r"template_mhy_bh3",
+    "BH3.JP": r"template_mhy_bh3",
+    "BH3.KR": r"template_mhy_bh3",
+    "BH3.SEA": r"template_mhy_bh3",
+    "BH3.EU_USA": r"template_mhy_bh3",
+    "Genshin": r"template_mhy_genshin",
+    "HKSR": r"template_mhy_starrail",
+    "ZZZ": r"template_mhy_zzz",
+    "HNA": r"template_mhy_nexusanima",
+    "PPT": r'template_mhy_petitplanet',
     "Varzapura": r"template_mhy_varsapura",
-    "TearsOfThemis": r"template_mhy_tot",
 }
